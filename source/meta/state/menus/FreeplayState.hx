f377a4e04c12e055e1398d6258f8e225fb53e996
bigcoqui <102246267+bigcoqui@users.noreply.github.com>
2022-10-26 11:20:32 -0300

    update FreeplayState.hx

--- source/meta/state/menus/FreeplayState.hx
+++ source/meta/state/menus/FreeplayState.hx
@@ -18,7 +18,6 @@ import openfl.utils.Assets as OpenFlAssets;
 import meta.MusicBeat.MusicBeatState;
 import meta.data.*;
 import meta.data.Song.SwagSong;
-import meta.data.dependency.Discord;
 import meta.data.font.Alphabet;
 import openfl.media.Sound;
 import sys.FileSystem;
@@ -121,10 +120,6 @@ class FreeplayState extends MusicBeatState
 				}
 			}
 		}
-
-		#if !html5
-		Discord.changePresence('FREEPLAY MENU', 'Main Menu');
-		#end
 
 		// LOAD CHARACTERS
 
